884254a47cb9423a27deade61069c35d408cce7e
Jeff Winget <jeff@3dgradebook.com>
2024-11-05 09:21:57 -0700

    Tweaks

--- hosts/specialisations/gnome.nix
+++ hosts/specialisations/gnome.nix
@@ -1,9 +1,13 @@
 { config, pkgs, ... }:
 {
+    imports = [
+
+    ];
+
     specialisation = {
         gnome-desktop.configuration = {
-        services.xserver.displayManager.gdm.enable = true;
-        services.xserver.desktopManager.gnome.enable = true;
+            services.xserver.displayManager.gdm.enable = true;
+            services.xserver.desktopManager.gnome.enable = true;
         };
     };
 }
