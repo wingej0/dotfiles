e63837dc23f0bb5b79dca0a1e0526e2022c36382
Jeff Winget <jeff@3dgradebook.com>
2024-11-19 07:10:31 -0700

    tweaks

--- hosts/modules/python.nix
+++ hosts/modules/python.nix
@@ -3,17 +3,17 @@
     environment.systemPackages = with pkgs; [
         # Python Environment
         (python3.withPackages (ps: with ps; [
-        requests
-        pip
-        numpy
-        pandas
-        jupyterlab
-        pymongo
-        matplotlib
-        matplotlib-venn
-        gspread
-        sqlalchemy
-        pyodbc
+            requests
+            pip
+            numpy
+            pandas
+            jupyterlab
+            pymongo
+            matplotlib
+            matplotlib-venn
+            gspread
+            sqlalchemy
+            pyodbc
         ]))
     ];
 }
