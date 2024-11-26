0e8588e6f0992c258055bb1ed19e0dab66f32af7
Jeff Winget <jeff@3dgradebook.com>
2024-11-26 08:54:06 -0700

    Set up remote desktop part 1,000,000

--- hosts/dis-winget/configuration.nix
+++ hosts/dis-winget/configuration.nix
@@ -68,19 +68,6 @@
     #media-session.enable = true;
   };
 
-  # Set up remote desktop https://nixos.wiki/wiki/Remote_Desktop
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-  services.xrdp.openFirewall = true;
-
-  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-  # If no user is logged in, the machine will power down after 20 minutes.
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
