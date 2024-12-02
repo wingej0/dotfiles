c3888cdabef95336ee884a4a99fc081b4ef3391b
Jeff Winget <jeff@3dgradebook.com>
2024-12-02 15:02:45 -0700

    fixed typo

--- hosts/dis-winget/configuration.nix
+++ hosts/dis-winget/configuration.nix
@@ -26,7 +26,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   
-  services.xserver.displayManager.ligthdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.cinnamon.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
